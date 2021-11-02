d5385cff40794eb6e5aed4a499a1c9f676fc8dbf
nakashi94 <n0904yBUNJOU@gmail.com>
2021-11-02 23:11:23 +0900

    Form useState

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = () => {
+  const [city, setCity] = useState<string>("");
   return (
     <form>
-      <input type="text" name="city" placeholder="city name"/>
+      <input type="text" name="city" placeholder="city name" onChange={e => setCity(e.target.value)}/>
+      {city}
       <button type="submit">GetWeather</button>
     </form>
   );
