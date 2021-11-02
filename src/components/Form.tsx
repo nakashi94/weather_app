417c6e82dc879d7f238975cb4af18b89cd8f72db
nakashi94 <n0904yBUNJOU@gmail.com>
2021-11-02 23:39:32 +0900

    WeatherAPI complete

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,13 +2,21 @@ import React, { useState } from 'react';
 
 const Form = () => {
   const [city, setCity] = useState<string>("");
+  const getWeather = (e: any) => { // eの型をanyで定義
+    e.preventDefault();
+      fetch("https://api.weatherapi.com/v1/current.json?key=768198b545ed483d9ee141432210211&q=London&aqi=no")
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
   return (
     <form>
       <input type="text" name="city" placeholder="city name" onChange={e => setCity(e.target.value)}/>
-      {city}
-      <button type="submit">GetWeather</button>
+      <button type="submit" onClick={getWeather}>GetWeather</button>
     </form>
   );
 };
 
 export default Form
+
+// Weather API
+// https://api.weatherapi.com/v1/current.json?key=768198b545ed483d9ee141432210211&q=London&aqi=no
