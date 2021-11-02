e773e3539cc62d857fc870905cab552f3712eaa5
nakashi94 <n0904yBUNJOU@gmail.com>
2021-11-03 01:46:50 +0900

    result Single Source of Truth

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import Title from './components/Title';
 import Form from './components/Form';
+import Result from './components/Result';
 import './App.css';
 
 function App() {
+  const [city, setCity] = useState<string>("");
+  const getWeather = (e: any) => { // eの型をanyで定義
+    e.preventDefault();
+      fetch("https://api.weatherapi.com/v1/current.json?key=768198b545ed483d9ee141432210211&q=London&aqi=no")
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
   return (
     <div className="test">
       <Title />
-      <Form />
+      <Form setCity={setCity} getWeather={getWeather} />
+      <Result />
     </div>
   );
 };
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,16 @@
 import React, { useState } from 'react';
 
-const Form = () => {
-  const [city, setCity] = useState<string>("");
-  const getWeather = (e: any) => { // eの型をanyで定義
-    e.preventDefault();
-      fetch("https://api.weatherapi.com/v1/current.json?key=768198b545ed483d9ee141432210211&q=London&aqi=no")
-      .then(res => res.json())
-      .then(data => console.log(data))
-  }
+type FormPropsType = {
+  setCity: React.Dispatch<React.SetStateAction<string>>;
+  getWeather: (e: any) => void; // any型を指定
+}
+
+const Form = (props: FormPropsType) => { // propsの型指定でany型を指定
+  
   return (
     <form>
-      <input type="text" name="city" placeholder="city name" onChange={e => setCity(e.target.value)}/>
-      <button type="submit" onClick={getWeather}>GetWeather</button>
+      <input type="text" name="city" placeholder="city name" onChange={e => props.setCity(e.target.value)}/>
+      <button type="submit" onClick={props.getWeather}>GetWeather</button>
     </form>
   );
 };
