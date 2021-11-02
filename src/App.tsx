c4ee2fa971cf16a6ff755b8f176fd43a52c59c9b
nakashi94 <n0904yBUNJOU@gmail.com>
2021-11-02 21:43:19 +0900

    Title-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import Title from './components/Title';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="test">
+      <Title />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Form.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Form = () => {
+  return (
+    <form>
+      <input type="text" name="city" placeholder="city name"/>
+      <button type="submit">GetWeather</button>
+    </form>
+  );
+};
+
+export default Form
